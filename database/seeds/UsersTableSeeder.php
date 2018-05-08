a4877f05ea1ca2aa999c6b5f79219ae34bf663a5
Le Minh Tri <ansidev@gmail.com>
2018-05-09 05:48:47 +0700

    Add database

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => "John Doe",
+            'email' => "johndoe@example.com",
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'api_token' => str_random(64),
+            'remember_token' => str_random(10),
+        ]);
+    }
+}
