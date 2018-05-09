83d54c12cb74f55e91a459e5ebffcfd1c901d90d
Le Minh Tri <ansidev@gmail.com>
2018-05-09 14:56:45 +0700

    Add helper.

---
+++ resources/assets/js/helpers/api.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+export function get(url, params = {}) {
+    return axios.get(url, {
+        headers: {
+            'Authorization': `Bearer ${window.apiToken}`
+        },
+        params: params
+    })
+}
+
+export function post(url, payload) {
+    return axios({
+        method: 'POST',
+        url: url,
+        data: payload,
+        headers: {
+            'Authorization': `Bearer ${window.apiToken}`
+        }
+    })
+}
+
+// delete is reserved keyword
+export function del(url) {
+    return axios({
+        method: 'DELETE',
+        url: url,
+        headers: {
+            'Authorization': `Bearer ${window.apiToken}`
+        }
+    })
+}
+
+export function interceptors(cb) {
+    axios.interceptors.response.use((res) => {
+        return res;
+    }, (err) => {
+        cb(err);
+        return Promise.reject(err)
+    })
+}
