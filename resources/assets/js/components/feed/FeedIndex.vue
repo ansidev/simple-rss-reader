259a4c1d7c9a63efbe21e284f7deabadb6a3a9a9
Le Minh Tri <ansidev@gmail.com>
2018-05-09 14:56:36 +0700

    Add Vue components.

---
+++ resources/assets/js/components/feed/FeedIndex.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <router-link :to="{name: 'feedCreate'}" class="btn btn-primary float-left">Create</router-link>
+            <div class="dropdown float-right">
+                <a class="btn btn-primary dropdown-toggle" href="#" role="button" id="categoryFilter"
+                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-text="selectedCategory">
+                </a>
+
+                <div class="dropdown-menu" aria-labelledby="categoryFilter">
+                    <a class="dropdown-item" :class="{'active': selectedCategory === 'All'}"
+                       href="#" @click="filterFeedByCategory('all')">All
+                    </a>
+                    <a v-for="category in categories.data" class="dropdown-item"
+                       :class="{'active': selectedCategory === category.name}"
+                       v-bind:category-id="category.id" href="#" @click="filterFeedByCategory(category)">
+                        {{ category.name }}
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <feed-list-item v-for="(feed, index) in currentFeeds.data"
+                                :key="index" :data="feed" :order="index"></feed-list-item>
+            </div>
+            <vue-pagination :pagination="currentFeeds" @paginate="getFeeds" :offset="4"></vue-pagination>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {get} from '../../helpers/api'
+    import {getCategories} from "../../helpers/categories";
+    import FeedListItem from './FeedListItem'
+
+    export default {
+        mounted() {
+            this.getFeeds();
+            getCategories().then(response => {
+                this.categories = response
+            });
+        },
+        data: function () {
+            return {
+                feeds: [],
+                currentFeeds: [],
+                categories: [],
+                selectedCategory: 'All',
+                selectedCategoryId: null
+            }
+        },
+        components: {
+            FeedListItem
+        },
+        methods: {
+            getFeeds() {
+                let params = {};
+                if (typeof this.currentFeeds.current_page !== 'undefined') {
+                    params.page = this.currentFeeds.current_page;
+                }
+                if (this.selectedCategoryId !== null) {
+                    params.categoryId = this.selectedCategoryId
+                }
+                get('/api/feeds', params)
+                    .then((response) => {
+                        this.feeds = response.data.feeds;
+                        this.currentFeeds = Object.assign({}, this.feeds);
+                    })
+                    .catch(() => {
+                        console.log('handle server error from here');
+                    });
+            },
+            filterFeedByCategory(category) {
+                if (category === 'all') {
+                    this.selectedCategory = 'All';
+                    this.selectedCategoryId = null;
+                } else {
+                    this.selectedCategory = category.name;
+                    this.selectedCategoryId = category.id;
+                }
+                this.currentFeeds.current_page = 1;
+                this.getFeeds();
+            }
+        }
+    }
+</script>
---
+++ resources/assets/js/components/feed/FeedListItem.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="col-md-6">
+        <div class="card flex-md-row mb-4 box-shadow h-md-250 feed-card">
+            <div class="card-body d-flex flex-column align-items-start">
+                <strong class="d-inline-block mb-2 text-primary">{{ data.category.name }}</strong>
+                <h3 class="mb-0">
+                    <a class="text-dark" :href="data.link">{{ data.title }}</a>
+                </h3>
+                <div class="mb-1 text-muted">{{ data.published_at }}</div>
+                <p class="card-text mb-auto">{{ data.description }}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                        <a class="btn btn-success" :href="data.link" role="button">Read</a>
+                        <a class="btn btn-warning" :href="'/feeds/' + data.id + '/edit'" role="button">Edit</a>
+                        <a class="btn btn-danger" :href="'/feeds/' + data.id + '/delete'" role="button">Delete</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            data: {
+                required: true
+            }
+        },
+        mounted() {
+            console.log('Component mounted.');
+        }
+    }
+</script>
+
+<style scoped>
+    .feed-card {
+        height: 300px;
+    }
+</style>
