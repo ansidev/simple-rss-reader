820bcda4e321f3ded461a354e32b1da0ae56dec2
Le Minh Tri <ansidev@gmail.com>
2018-12-17 08:36:32 +0700

    Fix bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('REDIRECT_HTTPS')) {
-            $url->formatScheme('https');
-        }
+        //
     }
 
     /**
